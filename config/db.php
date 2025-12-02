b2d9791a62ef51eeffb38373f3b96657a924835f
Uha kruthi Kommi <uhakruthi1@gmail.com>
2025-12-02 11:00:01 -0500

    Add Craft project

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'driver' => 'mysql',
+    'server' => '127.0.0.1',
+    'port' => 3306,
+    'database' => 'recipe_book',
+    'user' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_general_ci',
+    'schema' => 'public',
+    'tablePrefix' => '',
+];
---
+++ config/guest-entries.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'enableCsrfProtection' => true,
+    'entryVariable' => 'entry',
+    'sections' => [
+        [
+            'sectionUid' => 'c88a916c-f8fb-4353-9fc9-2b0a6d168e4b',
+            'allowGuestSubmissions' => true,
+            'enabledByDefault' => true,
+            'runValidation' => true,
+            'authorUid' => 'dbe67fe7-53d7-4865-ba5e-e0a2d7d34a0c',
+        ],
+    ],
+];
